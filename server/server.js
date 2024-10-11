8832d573ada6d2feae24ceaf483cd3184ccf038e
Enes Arslan <enes.arslan@lenasoftware.com>
2024-10-11 14:04:41 +0300

    created project

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let rooms = {};
+
+wss.on('connection', (ws) => {
+    console.log('Bir kullanıcı bağlandı');
+
+    ws.on('message', (data) => {
+        const message = JSON.parse(data);
+        const room = message.room;
+        const text = message.text;
+
+        // Oda oluştur veya odaya ekle
+        if (!rooms[room]) {
+            rooms[room] = [];
+        }
+
+        rooms[room].forEach(client => {
+            if (client !== ws) {
+                client.send(JSON.stringify({ text, room }));
+            }
+        });
+
+        rooms[room].push(ws);
+    });
+
+    ws.on('close', () => {
+        console.log('Bir kullanıcı ayrıldı');
+        // Bağlantı koptuğunda odadan çıkar
+        for (let room in rooms) {
+            rooms[room] = rooms[room].filter(client => client !== ws);
+        }
+    });
+});
+
+server.listen(3001, () => {
+    console.log('Server 3001 portunda çalışıyor');
+});
